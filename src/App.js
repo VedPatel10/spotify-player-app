1a30036fe8839a598cc955f91701defc51815fb4
Ved <vedp7900@gmail.com>
2024-02-23 02:21:39 -0500

    updated viewing experience for different sized screens

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import SpotifyWebApi from 'spotify-web-api-js';
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import queryString from 'query-string';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import WelcomePage from './WelcomePage';
@@ -8,7 +8,7 @@ import ProfileStatsPage from './ProfileStatsPage';
 import PlaySearchPage from './PlaySearchPage';
 
 const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
-const spotifyApi = new SpotifyWebApi(); 
+const spotifyApi = new SpotifyWebApi();
 
 const App = () => {
   const [accessToken, setAccessToken] = useState('');
@@ -32,16 +32,16 @@ const App = () => {
         setFollowers(response.followers.total)
       })
 
-      spotifyApi.getMyTopArtists({time_range: "long_term", limit: 10, offset: 0})
+      spotifyApi.getMyTopArtists({ time_range: "long_term", limit: 10, offset: 0 })
         .then((response) => {
           setTopArtists(response.items)
-          
+
         })
         .catch((error) => {
           console.error('Error fetching top artists:', error);
         });
-      
-      spotifyApi.getMyTopTracks({time_range: "long_term", limit: 10, offset: 0})
+
+      spotifyApi.getMyTopTracks({ time_range: "long_term", limit: 10, offset: 0 })
         .then((response) => {
           setTopTracks(response.items)
         })
@@ -66,27 +66,27 @@ const App = () => {
   return (
     <Router>
       <div>
-          {!accessToken ? (
-            <button onClick={handleLogin}>Login with Spotify</button>
-          ) : (
-            <div>
-              {/* // APP COMPONENTS AFTER LOGIN SUCCESSFULLY */}
-              <Switch>
-                <Route exact path='/'>
-                  <WelcomePage name={displayName} picture={profilePicture} followers={followers}/>
-                </Route>
-            
-                <Route exact path='/profile-stats'>
-                  <ProfileStatsPage name={displayName} picture={profilePicture} topArtists={topArtists} topTracks={topTracks} />
-                </Route>
-              
-                <Route exact path='/play-search'>
-                  <PlaySearchPage name={displayName} picture={profilePicture} accesstoken={accessToken}/>
-                </Route>
-              </Switch>
-            </div>
-          )
-          }     
+        {!accessToken ? (
+          <button onClick={handleLogin} class="loginButton">Login with Spotify</button>
+        ) : (
+          <div>
+            {/* APP COMPONENTS AFTER LOGIN SUCCESSFULLY  */}
+            <Switch>
+              <Route exact path='/'>
+                <WelcomePage name={displayName} picture={profilePicture} followers={followers} />
+              </Route>
+
+              <Route exact path='/profile-stats'>
+                <ProfileStatsPage name={displayName} picture={profilePicture} topArtists={topArtists} topTracks={topTracks} />
+              </Route>
+
+              <Route exact path='/play-search'>
+                <PlaySearchPage name={displayName} picture={profilePicture} accesstoken={accessToken} />
+              </Route>
+            </Switch>
+          </div>
+        )
+        }
       </div>
     </Router>
   );
